0e0ab6626c75524be870064902e9dcb21576b06e
Brenden <brenden.iannetta@mytools2go.ca>
2022-09-15 17:29:48 -0400

    test

---
+++ pseudocode.py
@@ -0,0 +1,43 @@
+# 1. Determine if two numbers are equal. If they are equal print "same" otherwise print "different".
+
+# num1 = any number1
+# num2 = any number2
+# 
+# if num1 is the same as num2
+#   print "same"
+# else
+#   print "different"
+
+# 2. Write pseudo code that directs someone to walk from your seat to the door.
+# 
+# stand up
+# step left out of desk
+# walk straight until path is blocked
+# turn 30 degrees right
+# walk straight until door is blocking you
+
+# 3. Write pseudo code that asks two users for rock, paper, scissors and compares them to see who won.
+# 
+# user1 = rock, paper or scissors
+# user2 = rock, paper or scissors
+# 
+# if user1 is the same as user2:
+#   print no winner
+# 
+# if user1 is scissors:
+#   if user2 is rock:
+#       print user2 wins
+#   else:
+#       print user1 wins
+# 
+# if user1 is rock:
+#   if user2 is paper:
+#       print user2 wins
+#   else:
+#       print user1 wins
+# 
+# if user1 is paper:
+#   if user2 is scissors:
+#       print user2 wins
+#   else:
+#       print user1 wins
